eddd9eb4b786fe95fdcbaca7a5c58e5a1a6f8435
Emmanuel <emmco96@gmail.com>
2023-10-11 17:50:43 +0100

    new

---
+++ index.js
@@ -0,0 +1,24 @@
+const countryFlags = ['images/usa.jpg', 'images/uk.jpg', 'images/uae.png', 'images/australia.png', 'images/canada.png', 'images/germany.png','images/italy.png']; // Add the paths to your flag images
+
+const flagContainerInner = document.querySelector('.flag-container-inner');
+
+function createFlagElement(flagPath) {
+    const flagElement = document.createElement('img');
+    flagElement.src = flagPath;
+    flagElement.classList.add('flag');
+    return flagElement;
+}
+
+// Duplicate the images to ensure seamless rolling effect
+const duplicatedFlags = [...countryFlags, ...countryFlags];
+
+duplicatedFlags.forEach(flagPath => {
+    const flagElement = createFlagElement(flagPath);
+    flagContainerInner.appendChild(flagElement);
+});
+
+function rollFlags() {
+    flagContainerInner.style.animation = `roll ${duplicatedFlags.length * 2}s linear infinite`; // Set the animation duration based on the number of duplicated flags
+}
+
+rollFlags();
