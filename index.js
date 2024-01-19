f70d8a862a95ffcb3f813d8da7968e857423961e
unknown <emmco96@gmail.com>
2024-01-19 18:24:11 +0100

    new

--- index.js
+++ index.js
@@ -37,3 +37,23 @@ rollFlags();
 
 
 
+
+function myFunction(x) {
+  x.classList.toggle("change");
+}
+
+var open = false;
+
+function openNav() {
+  var sideNav = document.getElementById("mySidenav");
+  
+  if (sideNav.style.width === "0px" || sideNav.style.width === "") {
+      sideNav.style.width = "250px";
+      open = true;
+  } else {
+      sideNav.style.width = "0";
+      open = false;
+  }
+}
+
+
